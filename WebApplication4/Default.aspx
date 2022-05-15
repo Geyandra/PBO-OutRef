c8c7ffca1ee13aeed84a50097180fe1203931c56
geyandra <geyandra.g.k@gmail.com>
2022-05-15 21:25:41 +0700

    Add project files.

---
+++ WebApplication4/Default.aspx
@@ -0,0 +1,160 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Tugas_PBO.Poli" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        #btnInsertion {
+            background-color: rgb(80, 73, 136);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        #btnUpdation {
+            background-color: rgb(71 146 119);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        #btnSelect{
+            background-color: rgb(78 111 152);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        #btnDelete{
+            background-color: rgb(186 96 44);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        input[type=text]{
+            border-radius: 10px;
+            padding: 10px;
+        }
+        form{
+            display: flex;
+            flex-direction: row;
+            align-items: start;
+        }
+        table{
+            margin-right: 50px;
+            margin-left: 50px;
+        }
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+            <table>
+                <tr>
+                    <td colspan="2">
+                        <center><h1>&nbsp;Create/Update</h1>
+                            <p>&nbsp;</p></center>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style7"><p align="right">Poli ID&nbsp;&nbsp;&nbsp; </p></td>
+                    <td class="auto-style1">:&nbsp;&nbsp;<asp:TextBox ID="txtPoliID" runat="server"></asp:TextBox>
+                    </td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style7"><p align="right">Nama Poli&nbsp;&nbsp;&nbsp; </p></td>
+                    <td class="auto-style1">:&nbsp;
+                        <asp:TextBox ID="txtNamaPoli" runat="server"></asp:TextBox>
+                    </td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style7"><p align="right">Jumlah Dokter&nbsp;&nbsp;&nbsp; </p></td>
+                    <td class="auto-style1">:&nbsp;
+                        <asp:TextBox ID="txtJumlahDokter" runat="server"></asp:TextBox>
+                    </td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style7">
+                        <br />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <center><asp:Button ID="btnInsertion" runat="server" Text="Create" OnClick="btnInsertion_Click" CssClass="auto-style3" Width="142px"  />
+                            <br />
+                            <br />
+                        </center>
+                        <center><asp:Button ID="btnUpdation" runat="server" Text="Update" OnClick="btnUpdation_Click" Width="142px"  />
+                            <br />
+                            <br />
+                        </center>
+                        <center><asp:Label ID="lblmsg" runat="server" ForeColor="#83BD75"></asp:Label></center>
+                        <center><asp:Label ID="lblmsg2" runat="server" ForeColor="red"></asp:Label></center>
+                    </td>
+                </tr>
+            </table>
+
+            <table>
+                <tr>
+                    <td class="auto-style5">
+                        <center><h1 class="auto-style4">Show all Poli Data</h1></center>
+                    </td>
+                </tr>
+                <tr>
+
+                    <td class="auto-style5">
+                        <center><asp:Button ID="btnSelect" runat="server" Text="Show" Width="142px" OnClick="btnSelect_Click" /></center>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style5">
+                        <center><asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="493px" ></asp:GridView></center>
+                    </td>
+                </tr>
+            </table>
+           
+            <table>
+                <tr>
+                    <td colspan="3">
+                        <center><h1>Delete Account</tr> <tr></center>
+                    <tr>
+                    <td class="auto-style7"><p align="right">Poli ID&nbsp;&nbsp;&nbsp; </p></td>
+                    <td class="auto-style1">:&nbsp;&nbsp;<asp:TextBox ID="txtPolID" runat="server"></asp:TextBox>
+                    </td>
+                        <br />
+                    </td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+                </tr>
+                <tr >
+
+                    <td colspan="3">
+                        <center>
+                            <br />
+                            <asp:Button ID="btnDelete" runat="server" Text="Delete" Width="142px" OnClick="btnDelete_Click" /></center>
+                        <center>
+                            <br />
+                            <asp:Label ID="lblmessage" runat="server" ForeColor="#83BD75"></asp:Label></center>
+                        <center>
+                            <asp:Label ID="lblmessage2" runat="server" ForeColor="Red"></asp:Label></center>
+                    </td>
+                </tr>
+                
+            </table>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/Default.aspx.cs
@@ -0,0 +1,168 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tugas_PBO
+{
+    public partial class Poli : System.Web.UI.Page
+    {
+        protected void btnInsertion_Click(object sender, EventArgs e)
+        {
+            if (SqlDBHelper.ExecuteNonQuery("Insert into poli values(@id_poli,@nama_poli,@jumlah_dokter)", CommandType.Text, new NpgsqlParameter("@id_poli", Convert.ToInt32(txtPoliID.Text)), new NpgsqlParameter("@nama_poli", txtNamaPoli.Text), new NpgsqlParameter("@jumlah_dokter", Convert.ToInt32(txtJumlahDokter.Text))))
+            {
+                txtPoliID.Text = ""; txtNamaPoli.Text = ""; txtJumlahDokter.Text = "";
+                GridView1.Visible = false;
+                lblmessage2.Text = String.Empty;
+                lblmessage.Text = String.Empty;
+                lblmsg2.Text = String.Empty;
+                lblmsg.Text = "Data Has been Saved";
+            }
+
+            else
+            {
+                txtPoliID.Text = ""; txtNamaPoli.Text = ""; txtJumlahDokter.Text = "";
+                GridView1.Visible = false;
+                lblmessage2.Text = String.Empty;
+                lblmessage.Text = String.Empty;
+                lblmsg.Text = String.Empty;
+                lblmsg2.Text = "Failed, ID Should Be Unique";
+            }
+
+        }
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            DataTable dt;
+            if (SqlDBHelper.ExecuteDataSet(out dt, "Select * from poli order by id_poli", CommandType.Text))
+            {
+                lblmsg.Text = String.Empty;
+                lblmsg2.Text = String.Empty;
+                lblmessage.Text = String.Empty;
+                lblmessage2.Text = String.Empty;
+                GridView1.DataSource = dt;
+                GridView1.Visible = true;
+                GridView1.DataBind();
+            }
+        }
+        protected void btnUpdation_Click(object sender, EventArgs e)
+        {
+            if (SqlDBHelper.ExecuteNonQuery("update poli set id_poli=@id_poli, nama_poli=@nama_poli, jumlah_dokter=@jumlah_dokter where id_poli=@id_poli", CommandType.Text, new NpgsqlParameter("@id_poli", Convert.ToInt32(txtPoliID.Text)), new NpgsqlParameter("@nama_poli", txtNamaPoli.Text), new NpgsqlParameter("@jumlah_dokter", Convert.ToInt32( txtJumlahDokter.Text))))
+            {
+                txtNamaPoli.Text = ""; txtPoliID.Text = ""; txtJumlahDokter.Text = "";
+                GridView1.Visible = false;
+                lblmsg2.Text = String.Empty;
+                lblmsg.Text = "Data Has been Updated";
+            }
+            else
+            {
+                txtNamaPoli.Text = ""; txtPoliID.Text = ""; txtJumlahDokter.Text = "";
+                GridView1.Visible = false;
+                lblmsg2.Text = String.Empty;
+                lblmsg2.Text = "Failed, ID Should Be Unique";
+            }
+
+        }
+
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string var;
+            var = txtPolID.Text;
+            if (var == "")
+            {
+                txtPolID.Text = "";
+                GridView1.Visible = false;
+                lblmsg.Text = String.Empty;
+                lblmsg2.Text = String.Empty;
+                lblmessage.Text = String.Empty;
+                lblmessage2.Text = "Please Write the Correct ID";
+            }
+            else if (SqlDBHelper.ExecuteNonQuery("Delete from poli where id_poli=@id_poli", CommandType.Text, new NpgsqlParameter("@id_poli", Convert.ToInt32(txtPolID.Text))))
+            {
+                txtPolID.Text = "";
+                GridView1.Visible = false;
+                lblmsg.Text = String.Empty;
+                lblmsg2.Text = String.Empty;
+                lblmessage2.Text = String.Empty;
+                lblmessage.Text = "Data Has been Deleted";
+            }
+        }
+        public static class SqlDBHelper
+        {
+            public static bool ExecuteDataSet(out DataTable dt, string sql, CommandType cmdType, params NpgsqlParameter[] parameters)
+            {
+                using (DataSet ds = new DataSet())
+                using (NpgsqlConnection connStr = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString))
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connStr))
+                {
+                    cmd.CommandType = cmdType;
+                    foreach (var item in parameters)
+                    {
+                        cmd.Parameters.Add(item);
+                    }
+
+                    try
+                    {
+                        cmd.Connection.Open();
+                        new NpgsqlDataAdapter(cmd).Fill(ds);
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        dt = null;
+                        return false;
+
+                    }
+
+
+                    dt = ds.Tables[0];
+                    return true;
+                }
+            }
+            public static bool ExecuteNonQuery(string sql, CommandType cmdType, params NpgsqlParameter[] parameters)
+            {
+                using (DataSet ds = new DataSet())
+                using (NpgsqlConnection connStr = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString))
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connStr))
+                {
+                    cmd.CommandType = cmdType;
+                    foreach (var item in parameters)
+                    {
+                        cmd.Parameters.Add(item);
+                    }
+
+                    try
+                    {
+
+                        cmd.Connection.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (NpgsqlException ex)
+                    {
+
+                        return false;
+
+                    }
+
+                    return true;
+
+                }
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
